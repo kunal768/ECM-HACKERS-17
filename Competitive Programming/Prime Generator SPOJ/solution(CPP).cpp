4cc07c23bb66b9bda5eacb8e7cdd23af380b4ce3
kunal768 <33108756+kunal768@users.noreply.github.com>
2018-10-14 23:40:02 +0530

    Add files via upload

---
+++ Competitive Programming/Prime Generator SPOJ/solution(CPP).cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+#include<math.h>
+#include <iostream>  
+using namespace std;  
+int isPrime(int n)  
+{  
+  int i, flag=0;    
+  for(i = 2; i <= sqrt(n); i++)  
+  {  
+      if(n % i == 0)  
+      {  
+          flag=1;  
+          break;  
+      }  
+  }  
+  if (flag==0)  
+  return 0;
+  else if(flag ==1)
+    return 1;  
+}  
+
+int main() {
+  int t,i;cin>>t;
+  for(i = 0;i<t;i++){
+    int m,n,j;cin>>m>>n;
+    for(j=m;j<=n;j++){
+      if(j == 1)
+        continue;
+      if(isPrime(j)==0)
+         cout<<j<<"\n";
+    }
+  }
+}
+
