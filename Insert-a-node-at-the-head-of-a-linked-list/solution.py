fc16463f3d4e64895726cfb604ef5c8eabf60f2b
kunal768 <33108756+kunal768@users.noreply.github.com>
2018-10-19 00:23:09 +0530

    Delete solution.py

--- Insert-a-node-at-the-head-of-a-linked-list/solution.py
+++
@@ -1,9 +0,0 @@
-
-#*************Our Code ******************************
-
-def insertNodeAtHead(llist_head,data):
-    temp = SinglyLinkedListNode(data)
-    temp.next = llist_head
-    llist_head = temp
-    return llist_head
-
